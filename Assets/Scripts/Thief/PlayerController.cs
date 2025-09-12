74377e99431698116f0f094e25eb7c5d88a607a3
Daniel Levy <levy.d6@gmail.com>
2025-09-11 23:21:08 -0500

    Camera working

---
+++ Assets/Scripts/Thief/PlayerController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+    [DefaultExecutionOrder(-1)]
+    public class PlayerController : MonoBehaviour
+    {
+        [Header("Components")]
+#nullable disable
+        [SerializeField] private CharacterController _characterController;
+        [SerializeField] private Camera _playerCamera;
+#nullable enable
+
+        [Header("Base Movement")]
+        public float runAcceleration = 50f;
+        public float runSpeed = 4f;
+        public float drag = 20f;
+
+        [Header("Camera Settings")]
+        public float lookSenseH = 0.1f;
+        public float lookSenseV = 0.1f;
+        public float lookLimitV = 89f;
+
+#nullable disable
+        private PlayerLocomotionInput _playerLocomotionInput;
+#nullable enable
+        private Vector2 _cameraRotation = Vector2.zero;
+        private Vector2 _playerTargetRotation = Vector2.zero;
+
+        private void Awake()
+        {
+            _playerLocomotionInput = GetComponent<PlayerLocomotionInput>();
+        }
+        private void Update()
+        {
+            Vector3 cameraForwardXZ = new Vector3(_playerCamera.transform.forward.x, 0f, _playerCamera.transform.forward.z).normalized;
+            Vector3 cameraRightXZ = new Vector3(_playerCamera.transform.right.x, 0f, _playerCamera.transform.right.z).normalized;
+            Vector3 movementDirection = cameraRightXZ * _playerLocomotionInput.MovementInput.x + cameraForwardXZ * _playerLocomotionInput.MovementInput.y;
+
+            Vector3 movementDelta = movementDirection * runAcceleration * Time.deltaTime;
+            Vector3 newVelocity = _characterController.velocity + movementDelta;
+
+            // Add drag to player
+            Vector3 currentDrag = newVelocity.normalized * drag * Time.deltaTime;
+            newVelocity = (newVelocity.magnitude > drag * Time.deltaTime) ? newVelocity - currentDrag : Vector3.zero;
+            newVelocity = Vector3.ClampMagnitude(newVelocity, runSpeed);
+
+            // Move character (Unity suggests only calling this once per tick)
+            _characterController.Move(newVelocity * Time.deltaTime);
+        }
+
+        private void LateUpdate()
+        {
+            _cameraRotation.x += lookSenseH * _playerLocomotionInput.LookInput.x;
+            _cameraRotation.y = Mathf.Clamp(_cameraRotation.y - lookSenseV * _playerLocomotionInput.LookInput.y, -lookLimitV, lookLimitV);
+
+            _playerTargetRotation.x += transform.eulerAngles.x + lookSenseH * _playerLocomotionInput.LookInput.x;
+            transform.rotation = Quaternion.Euler(0f, _playerTargetRotation.x, 0f);
+
+            _playerCamera.transform.rotation = Quaternion.Euler(_cameraRotation.y, _cameraRotation.x, 0f);
+        }
+    }
---
+++ Assets/Scripts/Thief/PlayerLocomotionInput.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+    [DefaultExecutionOrder(-2)]
+    public class PlayerLocomotionInput : MonoBehaviour, PlayerControls.IPlayerLocomotionMapActions
+    {
+#nullable disable
+        public PlayerControls PlayerControls { get; private set; }
+#nullable enable
+        public Vector2 MovementInput { get; private set; }
+        public Vector2 LookInput { get; private set; }
+
+        private void OnEnable()
+        {
+            PlayerControls = new PlayerControls();
+            PlayerControls.Enable();
+
+            PlayerControls.PlayerLocomotionMap.Enable();
+            PlayerControls.PlayerLocomotionMap.SetCallbacks(this);
+        }
+
+        private void OnDisable()
+        {
+            PlayerControls.PlayerLocomotionMap.Disable();
+            PlayerControls.PlayerLocomotionMap.RemoveCallbacks(this);
+        }
+
+        public void OnMovement(InputAction.CallbackContext context)
+        {
+            MovementInput = context.ReadValue<Vector2>();
+        }
+
+        public void OnLook(InputAction.CallbackContext context)
+        {
+            LookInput = context.ReadValue<Vector2>();
+        }
+    }
